8e4b9f9d099d4c49096b3557da8f35fcbb28cdcd
Elena Berezina <farfalina1908@gmail.com>
2023-03-02 18:25:04 +0200

    doing task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryContent = createGalleryEl(galleryItems);
+
+function createGalleryEl() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `
+    }
+
+    ).join("");
+}
+gallery.innerHTML = galleryContent;
+
+gallery.addEventListener("click", onLinkClick);
+
+function onLinkClick(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img width="1400" height="900" src="${event.target.dataset.source}">
+`);
+instance.show();
+
+window.addEventListener("keydown", onEscClose);
+function onEscClose(event) {
+if (event.code === "Escape") {
+instance.close();
+}
+}
+}
+
